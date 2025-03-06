05eb8092aae14f7bd5e217995113f7645d4aecbd
meliaph-monitech <meliaph.monitech@gmail.com>
2025-03-06 13:01:58 +0900

    Update 250306_SignalAnoD_7.py

--- 250306_SignalAnoD_7.py
+++ 250306_SignalAnoD_7.py
@@ -111,7 +111,7 @@ def extract_advanced_features(signal):
 
 
 st.set_page_config(layout="wide")
-st.title("Laser Welding Anomaly Detection")
+st.title("Laser Welding Anomaly Detection V7 - Global Analysis")
 
 with st.sidebar:
     uploaded_file = st.file_uploader("Upload a ZIP file containing CSV files", type=["zip"])
